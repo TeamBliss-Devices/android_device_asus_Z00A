7dca0f36472aa899473eccf2c17be8e52a94ff25
BitO BSessiOn <bitobsession@gmail.com>
2015-09-19 13:35:08 +0200

    Blissified
    Change-Id: I3302c8e201f64eabcf8b5d55a3dc29d0be1c0da2

---
+++ bliss.mk
@@ -0,0 +1,31 @@
+# Boot animation
+TARGET_SCREEN_HEIGHT := 1080
+TARGET_SCREEN_WIDTH := 1920
+
+# Inherit some common CM stuff.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+
+$(call inherit-product, vendor/bliss/config/common_full_phone.mk)
+
+# Inherit device configuration
+$(call inherit-product, device/asus/Z00A/device.mk)
+
+# Enhanced NFC
+$(call inherit-product, vendor/bliss/config/nfc_enhanced.mk)
+
+DEVICE_PACKAGE_OVERLAYS += device/asus/Z00A/overlay
+
+PRODUCT_RUNTIMES := runtime_libart_default
+
+## Device identifier. This must come after all inclusions
+PRODUCT_NAME := bliss_Z00A
+PRODUCT_BRAND := asus
+PRODUCT_MODEL := ASUS_Z00A
+PRODUCT_MANUFACTURER := asus
+PRODUCT_DEVICE := Z00A
+
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    PRODUCT_NAME=WW_Z00A 
+#\
+#    BUILD_FINGERPRINT=asus/WW_Z00A/Z00A:5.0/LRX21V/2.19.40.22_20150627_5104_user:user/release-keys \
+#    PRIVATE_BUILD_DESC="asusmofd_fhd-user 5.0 LRX21V 2.19.40.22_20150627_5104_user release-keys"
